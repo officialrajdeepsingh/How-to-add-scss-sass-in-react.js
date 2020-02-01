786c8ac2cab243340800a6881f734d23c6c8b2ff
Rajdeep singh <32416397+officialrajdeepsingh@users.noreply.github.com>
2020-02-01 14:53:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import "./styles.scss";
+
+export default function App() {
+  return (
+    <>
+    <div className="card">
+      <img className='image' src="https://source.unsplash.com/random" alt=' use image by unspash API '/>
+      <div className='cardBody'> 
+        <h2> demo for scss </h2>
+        <p> According to Wikipedia sass is a preprocessor scripting language that is interpreted or compiled into Cascading Style Sheets (CSS).</p>
+        <button> Read More</button>
+      </div>
+    </div>
+    <div className="card">
+      <img className='image' src="https://source.unsplash.com/random" alt=' use image by unspash API '/>
+      <div className='cardBody'> 
+        <h2> demo for scss </h2>
+        <p> According to Wikipedia sass is a preprocessor scripting language that is interpreted or compiled into Cascading Style Sheets (CSS).</p>
+        <button> Read More</button>
+      </div>
+    </div>
+    <div className="card">
+      <img className='image' src="https://source.unsplash.com/random" alt=' use image by unspash API '/>
+      <div className='cardBody'> 
+        <h2> demo for scss </h2>
+        <p> According to Wikipedia sass is a preprocessor scripting language that is interpreted or compiled into Cascading Style Sheets (CSS).</p>
+        <button> Read More</button>
+      </div>
+    </div>
+    </>
+  );
+}
